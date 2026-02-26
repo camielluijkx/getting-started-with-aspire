b2230615f9d548e5f79d081220d7e40a345cc332
Camiel Luijkx <camiel.luijkx@gmail.com>
2026-02-26 14:17:09 +0100

    reuse existing postgresql instance

--- BookStoreAspire.AppHost/AppHost.cs
+++ BookStoreAspire.AppHost/AppHost.cs
@@ -1,14 +1,21 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var postgres = builder
-    .AddPostgres("postgres")
-    .WithDataVolume("app-postgres-data")
-    .AddDatabase("booksDb");
+//var postgres = builder
+//    .AddPostgres("postgres")
+//    .WithDataVolume("app-postgres-data")
+//    .AddDatabase("booksDb");
+
+//var apiService = builder
+//    .AddProject<Projects.BookStoreAspire_ApiService>("bookstore-api")
+//    .WithReference(postgres)
+//    .WaitFor(postgres)
+//    .WithHttpHealthCheck("/health");
+
+var booksDbConnection = builder.AddConnectionString("booksDb");
 
 var apiService = builder
     .AddProject<Projects.BookStoreAspire_ApiService>("bookstore-api")
-    .WithReference(postgres)
-    .WaitFor(postgres)
+    .WithReference(booksDbConnection)
     .WithHttpHealthCheck("/health");
 
 builder
