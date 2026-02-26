22115c80ea836f6b59dcc8ec6924549b3070e21d
Camiel Luijkx <camiel.luijkx@gmail.com>
2026-02-26 14:44:01 +0100

    seed initial book data

--- BookStoreAspire.ApiService/Migrations/20260226132922_SeedInitialBooks.Designer.cs
+++ BookStoreAspire.ApiService/Migrations/20260226132922_SeedInitialBooks.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using BookStoreAspire.ApiService;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace BookStoreAspire.ApiService.Migrations
 {
     [DbContext(typeof(BookStoreDbContext))]
-    partial class BookStoreDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260226132922_SeedInitialBooks")]
+    partial class SeedInitialBooks
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ BookStoreAspire.ApiService/Migrations/20260226132922_SeedInitialBooks.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BookStoreAspire.ApiService.Migrations
+{
+    public partial class SeedInitialBooks : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Books",
+                columns: ["Id", "Title", "Author", "Price"],
+                values: new object[,]
+                {
+                    {
+                        new Guid("11111111-1111-1111-1111-111111111111"),
+                        "Clean Code",
+                        "Robert C. Martin",
+                        39.99m
+                    },
+                    {
+                        new Guid("22222222-2222-2222-2222-222222222222"),
+                        "The Pragmatic Programmer",
+                        "Andrew Hunt and David Thomas",
+                        44.90m
+                    },
+                    {
+                        new Guid("33333333-3333-3333-3333-333333333333"),
+                        "Domain-Driven Design",
+                        "Eric Evans",
+                        59.00m
+                    },
+                    {
+                        new Guid("44444444-4444-4444-4444-444444444444"),
+                        "Refactoring",
+                        "Martin Fowler",
+                        49.50m
+                    },
+                    {
+                        new Guid("55555555-5555-5555-5555-555555555555"),
+                        "Design Patterns",
+                        "Erich Gamma, Richard Helm, Ralph Johnson, John Vlissides",
+                        54.75m
+                    }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValues:
+                [
+                    new Guid("11111111-1111-1111-1111-111111111111"),
+                    new Guid("22222222-2222-2222-2222-222222222222"),
+                    new Guid("33333333-3333-3333-3333-333333333333"),
+                    new Guid("44444444-4444-4444-4444-444444444444"),
+                    new Guid("55555555-5555-5555-5555-555555555555")
+                ]);
+        }
+    }
+}
